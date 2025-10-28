34b7c79403ee8d59981f71166c96797781e22f20
KoesJin <wlstjr2011@naver.com>
2025-10-29 03:42:01 +0900

    chore : 줄바꿈

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,42 @@
-import Dictaphone from "./Dictaphone";
-import { useSearchParams } from "react-router-dom";
-import { useDisplaySession } from "./hooks/useDisplaySession";
-import { useEffect } from "react";
+import Dictaphone from './Dictaphone';
+import { useSearchParams } from 'react-router-dom';
+import { useDisplaySession } from './hooks/useDisplaySession';
+import { useEffect } from 'react';
 
 function App() {
-  const [searchParams] = useSearchParams();
-  const uniqueId = searchParams.get("displayId") || "";
+    const [searchParams] = useSearchParams();
+    const uniqueId = searchParams.get('displayId') || '';
 
-  const { startSession, endSession, isConnected } = useDisplaySession(uniqueId);
+    const { startSession, endSession, isConnected } = useDisplaySession(uniqueId);
 
-  useEffect(() => {
-    if (uniqueId) {
-      startSession();
-    }
+    useEffect(() => {
+        if (uniqueId) {
+            startSession();
+        }
 
-    return () => {
-      endSession();
-    };
-  }, [uniqueId]);
+        return () => {
+            endSession();
+        };
+    }, [uniqueId]);
 
-  return (
-    <div>
-      {isConnected ? (
-        <Dictaphone displayId={uniqueId} />
-      ) : (
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            height: "100vh",
-          }}
-        >
-          연결중...
+    return (
+        <div>
+            {isConnected ? (
+                <Dictaphone displayId={uniqueId} />
+            ) : (
+                <div
+                    style={{
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        height: '100vh',
+                    }}
+                >
+                    연결중...
+                </div>
+            )}
         </div>
-      )}
-    </div>
-  );
+    );
 }
 
 export default App;
