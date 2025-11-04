f9472af9d21542dcde84b63bb4eafb580914635d
KoesJin <wlstjr2011@naver.com>
2025-11-04 23:01:08 +0900

    chore : 추적중지

--- src/env.js
+++ src/env.js
@@ -1,7 +1,4 @@
-export const SERVER_URL =
-  import.meta.env.VITE_SERVER_URL || "http://localhost:3000/api/graphql";
-export const WS_URL =
-  import.meta.env.VITE_WS_URL || "ws://localhost:3001/api/graphql";
-export const ASK_URL =
-  import.meta.env.VITE_ASK_URL || "http://localhost:3000/api/ask";
-export const ELEVEN_API_KEY = import.meta.env.VITE_ELEVENLABS_API_KEY || "";
+export const SERVER_URL = import.meta.env.VITE_SERVER_URL || 'https://app.woodi.life/api/graphql';
+export const WS_URL = import.meta.env.VITE_WS_URL || 'wss://app.woodi.life/ws';
+export const ASK_URL = import.meta.env.VITE_ASK_URL || 'https://app.woodi.life/api/ask';
+export const ELEVEN_API_KEY = import.meta.env.VITE_ELEVENLABS_API_KEY || '';
